b4f42d74108084bfbe57b80f753dc0d198a452d2
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-02-25 09:28:06 +0600

    style

---
+++ lib/Style/style.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const colorRed = Color.fromRGBO(231, 28, 36, 1);
+const colorGreen = Color.fromRGBO(33, 191, 115, 1);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1);
+const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1);
