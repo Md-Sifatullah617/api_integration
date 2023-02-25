1425ad8658c96a25d7bbc5b85fc85a8108ecc201
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-02-25 09:35:29 +0600

    decoration

--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -1,6 +1,21 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const colorRed = Color.fromRGBO(231, 28, 36, 1);
 const colorGreen = Color.fromRGBO(33, 191, 115, 1);
 const colorWhite = Color.fromRGBO(255, 255, 255, 1);
 const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1);
+
+InputDecoration appInputDecoration() {
+  return InputDecoration(
+      focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: colorGreen, width: 1)),
+      fillColor: colorWhite,
+      filled: true,
+      contentPadding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
+      enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: colorWhite, width: 0)),
+      border: const OutlineInputBorder(),
+      labelText: label);
+}
