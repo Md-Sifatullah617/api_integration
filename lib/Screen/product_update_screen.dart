091de1e0a6606807f26502777e8c02fea504519a
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-03 02:23:45 +0600

    dfg

--- lib/Screen/product_update_screen.dart
+++ lib/Screen/product_update_screen.dart
@@ -36,7 +36,60 @@ class _PUpdateScreenState extends State<PUpdateScreen> {
                 inputOnChange("ProductName", textValue);
               },
               decoration: appInputDecoration("ProductName"),
-            )
+            ),
+            TextFormField(
+              onChanged: (textValue) {
+                inputOnChange("ProductCode", textValue);
+              },
+              decoration: appInputDecoration("ProductCode"),
+            ),
+            TextFormField(
+              onChanged: (textValue) {
+                inputOnChange("Img", textValue);
+              },
+              decoration: appInputDecoration("Img"),
+            ),
+            TextFormField(
+              onChanged: (textValue) {
+                inputOnChange("UnitPrice", textValue);
+              },
+              decoration: appInputDecoration("UnitPrice"),
+            ),
+            TextFormField(
+              onChanged: (textValue) {
+                inputOnChange("TotalPrice", textValue);
+              },
+              decoration: appInputDecoration("TotalPrice"),
+            ),
+            appDropdownStyle(DropdownButton(
+                value: formValues["Qty"],
+                items: const [
+                      DropdownMenuItem(
+                        value: "",
+                        child: Text("Select Qt"),
+                      ),
+                      DropdownMenuItem(
+                        value: "1 pcs",
+                        child: Text("1 pcs"),
+                      ),
+                      DropdownMenuItem(
+                        value: "2 pcs",
+                        child: Text("2 pcs"),
+                      ),
+                      DropdownMenuItem(
+                        value: "3 pcs",
+                        child: Text("3 pcs"),
+                      ),
+                      DropdownMenuItem(
+                        value: "4 pcs",
+                        child: Text("4 pcs"),
+                      )], onChanged: (textValue) {
+                      inputOnChange("Qty", textValue);
+                    },
+                    isExpanded: true,
+                    underline: Container(),
+                    ))
+            
           ]),
         )
       ]),
