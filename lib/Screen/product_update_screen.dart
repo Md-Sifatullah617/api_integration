fbc3c3dc162bec0c050e7807609a858561869757
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-03 02:13:48 +0600

    fdg

--- lib/Screen/product_update_screen.dart
+++ lib/Screen/product_update_screen.dart
@@ -1,5 +1,5 @@
+import 'package:api_integraton/Style/style.dart';
 import 'package:flutter/material.dart';
-
 
 class PUpdateScreen extends StatefulWidget {
   const PUpdateScreen({super.key});
@@ -9,10 +9,37 @@ class PUpdateScreen extends StatefulWidget {
 }
 
 class _PUpdateScreenState extends State<PUpdateScreen> {
+  Map<String, String> formValues = {
+    "Img": "",
+    "ProductCode": "",
+    "ProductName": "",
+    "Qty": "",
+    "TotalPrice": "",
+    "UnitPrice": ""
+  };
+  inputOnChange(mapKey, textValue) {
+    setState(() {
+      formValues.update(mapKey, (value) => textValue);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Update Product")),
+      appBar: AppBar(title: const Text("Update Product")),
+      body: Stack(children: [
+        screenBackground(context),
+        Container(
+          child: Column(children: [
+            TextFormField(
+              onChanged: (textValue) {
+                inputOnChange("ProductName", textValue);
+              },
+              decoration: appInputDecoration("ProductName"),
+            )
+          ]),
+        )
+      ]),
     );
   }
 }
